95ad6d64090a0153ef433332a022ab73e4e0b4d9
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-02-18 16:15:40 +0100

    Added uncommitted directory (seemed to be causing an issue, though I didn't know that could be a problem).

---
+++ Sources/main_actor_reaction_managers/main_actor_reaction_managers.swift
@@ -0,0 +1,12 @@
+//
+//  main_actor_reaction_managers.swift
+//  
+//
+//  Created by Jeremy Bannister on 2/12/23.
+//
+
+///
+@_exported import MainActorValueModule_mapped_main_actor_reaction_manager
+@_exported import MainActorValueModule_main_actor_reaction_manager
+@_exported import MainActorValueModule_ergonomics_interface_main_actor_reaction_manager
+@_exported import MainActorValueModule_interface_main_actor_reaction_manager
