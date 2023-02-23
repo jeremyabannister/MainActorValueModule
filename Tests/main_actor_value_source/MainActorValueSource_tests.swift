fac17d44e47119300666c220cb45a276ea4f8e5d
Jeremy Bannister <jeremy.a.bannister@gmail.com>
2023-02-24 00:18:21 +0100

    Added `MainActorValueSource_tests.test_init_initialValue_withDeepChangeMonitoring()`.

--- Tests/main_actor_value_source/MainActorValueSource_tests.swift
+++ Tests/main_actor_value_source/MainActorValueSource_tests.swift
@@ -10,6 +10,63 @@ final class MainActorValueSource_tests: XCTestCase {
     
     ///
     @MainActor
+    func test_init_initialValue_withDeepChangeMonitoring () async throws {
+        
+        ///
+        let source =
+            MainActorValueSource(
+                initialValue: MainActorValueSource(initialValue: false),
+                withDeepChangeMonitoring: ()
+            )
+        
+        ///
+        let didSetOutput = MainActorValueSource<[Bool]>(initialValue: [])
+        
+        ///
+        source
+            .didSet
+            .registerReactionPermanently { output in
+                didSetOutput
+                    .mutateValue { $0.append(output.currentValue) }
+            }
+        
+        ///
+        try didSetOutput
+            .currentValue
+            .assertEqual(to: [])
+        
+        ///
+        source
+            .currentValue
+            .setValue(to: true)
+        
+        ///
+        try didSetOutput
+            .currentValue
+            .assertEqual(to: [true])
+        
+        ///
+        source
+            .currentValue
+            .setValue(to: true)
+        
+        ///
+        try didSetOutput
+            .currentValue
+            .assertEqual(to: [true, true])
+        
+        ///
+        source
+            .setValue(to: .init(initialValue: false))
+        
+        ///
+        try didSetOutput
+            .currentValue
+            .assertEqual(to: [true, true, false])
+    }
+    
+    ///
+    @MainActor
     func test_madeSubscribable () async throws {
         
         ///
@@ -35,7 +92,13 @@ final class MainActorValueSource_tests: XCTestCase {
             .assertEqual(to: [])
         
         ///
+        try await Task.sleep(seconds: 0.1)
+        
+        ///
         source.setValue(to: 8)
+        
+        ///
+        try await Task.sleep(seconds: 0.1)
         
         ///
         try reactionLog
